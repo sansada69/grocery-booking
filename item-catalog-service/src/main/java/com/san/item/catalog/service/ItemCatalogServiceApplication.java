de15a6be1ed8630261645c597e38cbe69fd5a934
Sanju Sadasivan <sansada69@gmail.com>
2024-02-27 01:07:04 +0530

    Initial commit for the following service;   1. auth-service   2. user-service   3. item-catalog-service

---
+++ item-catalog-service/src/main/java/com/san/item/catalog/service/ItemCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.san.item.catalog.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemCatalogServiceApplication.class, args);
+	}
+
+}
